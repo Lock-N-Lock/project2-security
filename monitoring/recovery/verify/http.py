b838ff8d9f3f2eb89af0cca0abf15a8c9ee7359c
KTCLIF <ktcloudljy@gmail.com>
2026-06-01 19:22:20 +0900

    Update http.py

--- monitoring/recovery/verify/http.py
+++ monitoring/recovery/verify/http.py
@@ -4,11 +4,7 @@ import requests
 def verify_http(url: str, timeout: int = 5) -> bool:
     try:
         response = requests.get(url, timeout=timeout)
-
-        if response.status_code == 200:
-            return True
-
-        return False
+        return response.ok
 
     except Exception:
         return False
