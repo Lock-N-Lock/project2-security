df60bbabbd088d3e789557070fbf168879dc1868
yimjongwon <awosung00@naver.com>
2026-06-17 15:11:12 +0900

    workflow_test

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -171,11 +171,13 @@ variable "app_image" {
 variable "db_host_replica" {
   description = "Replica DB의 IP (Tailscale IP)"
   type        = string
+  default     = "127.0.0.1"
 }
 
 variable "db_user" {
   description = "DB 사용자 이름"
   type        = string
+  default     = "lb-user"
 }
 
 variable "db_password" {
@@ -186,5 +188,11 @@ variable "db_password" {
 
 variable "db_name" {
   description = "DB 이름"
+  type        = string
+  default     = "lb-db"
+}
+
+variable "secret_key" {
+  description = "컨테이너 보안키"
   type        = string
 }
